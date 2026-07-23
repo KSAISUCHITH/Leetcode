6c9e37659be2319e2e127f1964ea6f65a3b59eb7
K Sai Suchith <ksuchith131@gmail.com>
2026-07-24 00:52:54 +0530

    Time: 0 ms (100%), Space: 44.4 MB (74.27%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max=Integer.MIN_VALUE;
+        int sum=0;
+
+        for(int row=0;row<accounts.length;row++){
+            for(int col=0;col<accounts[row].length;col++){
+                sum+=accounts[row][col];
+                if(sum>max){
+                    max=sum;
+                }
+            }
+            sum=0;
+        }
+
+        return max;
+    }
+}
