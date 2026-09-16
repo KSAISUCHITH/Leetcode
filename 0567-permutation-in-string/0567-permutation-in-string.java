967a000137d14949f316032787dbb90eb8f61c8c
K Sai Suchith <ksuchith131@gmail.com>
2026-09-17 01:12:22 +0530

    Time: 8 ms (49.15%), Space: 43.7 MB (96.61%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -17,7 +17,7 @@ class Solution {
                 s2count[s2.charAt(left)-'a']--;
                 left++;
             }
-            if(Arrays.equals(s1count,s2count)){
+            if((right-left+1 == s1.length())&&(Arrays.equals(s1count,s2count))){
                 return true;
             }
         }
