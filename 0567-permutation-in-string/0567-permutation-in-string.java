8168d0b42019d66490215f72478000a40d6dbcb9
K Sai Suchith <ksuchith131@gmail.com>
2026-09-17 01:10:45 +0530

    Time: 6 ms (87.36%), Space: 44.1 MB (48.09%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int [] s1count = new int[26];
+        int [] s2count = new int[26];
+
+        int left = 0;
+        for(int i = 0;i<s1.length();i++){
+            s1count[s1.charAt(i)-'a']++;
+        }
+
+
+        for(int right = 0;right<s2.length();right++){
+
+            s2count[s2.charAt(right)-'a']++;
+
+            while(right-left+1>s1.length()){
+                s2count[s2.charAt(left)-'a']--;
+                left++;
+            }
+            if((right-left+1 == s1.length())&&(Arrays.equals(s1count,s2count))){
+                return true;
+            }
+        }
+        return false;
+    }
+}
